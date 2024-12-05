c16962c14ab20b09f0fd86098911c65bb81ffa1d
varbinaryequals <kandra.yashwin@gmail.com>
2024-12-05 16:17:51 -0500

    Update script.js
    Added string-brute-forcing.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
+function getRandomChar() {
+    let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}[]-_=+\\|;:'"<>,.?/~ `
+    let index = Math.floor(Math.random() * chars.length)
+    return chars[index]
+}
+
+async function bruteForceString() {
+    let target = prompt("What would you like to brute-force?")
+    let result = ""
+    for (const letter of target) {
+        while (true) {
+            let i = getRandomChar()
+            document.getElementById("string").innerHTML = result + i
+            if (i === letter) {
+                result += i;
+                break;
+            }
+            await sleep(1);
+        }
+    }
+}
+
+function clearContent() {
+    document.getElementById("string").innerHTML = "";
+}
